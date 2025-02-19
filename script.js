aa0a981b2d2800bc20649396999a3ca3cf3cadd0
aminkaramvand <aminkaramvand70@gmail.com>
2025-02-20 00:25:21 +0330

    change API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = "0d6fb6888b251112cd86cfc53314a46f";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
